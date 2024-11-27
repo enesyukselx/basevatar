3868eb6b5f4c34344d8a58fb97cbf6823b37812d
enesyukselx <enesyuksel9903@gmail.com>
2024-11-27 15:29:20 +0300

    wip

--- app/(pages)/admin/votes/create/page.tsx
+++ app/(pages)/admin/votes/create/page.tsx
@@ -1,18 +1,18 @@
 import VoteForm from "../../components/Votes/VoteForm";
 
-const Page = async ({
-    searchParams,
-}: {
-    searchParams: {
-        day: string;
-    };
-}) => {
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+const Page = async (props: { searchParams: SearchParams }) => {
+    //
+    const searchParams = await props.searchParams;
+    const day = searchParams.day as string;
+
     return (
         <section>
             <div className="heading">
                 <h1 className="title">Create Vote</h1>
             </div>
-            <VoteForm day={searchParams.day ?? "1"} />
+            <VoteForm day={day ?? "1"} />
         </section>
     );
 };
