4ba9b3710998fde9111cdfbae191e628c02746de
enesyukselx <enesyuksel9903@gmail.com>
2025-04-15 11:41:54 +0300

    eslint configurations

--- apps/cronjob/eslint.config.mjs
+++ apps/cronjob/eslint.config.mjs
@@ -1,12 +1,4 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import { baseConfig } from "@basevatar/eslint-config";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default [...baseConfig];
